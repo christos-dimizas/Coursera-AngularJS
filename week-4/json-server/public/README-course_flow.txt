557e33e32727e86177e1ab76f38a532dcf1496ab
xdimizas <christos.dimizas@gmail.com>
2016-05-19 12:00:19 +0300

    Week-4-lesson-Client server | complete

---
+++ week-4/json-server/public/README-course_flow.txt
@@ -0,0 +1,90 @@
+
+
+/********************************************************************************************************/
+/********************************** WEEK 2 **************************************************************/
+
+Form validation 
+
+/********************************** /WEEK 2 ************************************************************/
+
+
+
+
+
+/*******************************************************************************************************/
+/********************************** WEEK 3 *************************************************************/
+
+
+
+/******** Angular Factory, Service and Dependency Injection
+
+Continue further code refactoring by separating the controllers & services in new files,
+injecting the services to the controllers and then implement the controllers to the app.js file.
+
+
+
+/******** Angular Templates
+
+- Directives (ng-model, ng-show, etc.)
+- Markups: {{ expression }}
+- Filters ()
+- Form controls ()
+
+EVERYTHING TAKES PLACE IN THE index.html file.
+
+
+
+/******** Angular ngRoute and Single Page Applications: Objectives and Outcomes
+
+Creates configurations which, according to the selected link/url specify which template and controller to use.
+
+What we had to do:
+
+1) app.js         : Add the required ngRoute configuration for all appropriate links.
+2) services.js    : Add _id:[number] to every dish in order to know which one it is (should come from the server with an id number)
+3) menu.html      : Add the appropriate #/menu/{{dish._id}} link to the img anchor href.
+4) controllers.js : Add $routeParams (as an dependence injection) to the controller responsible for each dish,
+                    and specify the $routeParams._id parameter to the dish variable.
+                    IMPORTANT: THE ORDER OF THE DEPENDENCIES ADDED TO THE FUNCTION MUST BE THE SAME WITH THOSE OF THE CONTROLLER.
+
+
+
+/********* Angular UI-Router and SPA: Objectives and Outcomes
+
+UI-Router module supports:
+ a) multiple & nested views in the same time (ng-Route module supports only one).
+ b) rendering different parts of the site using the routing.
+
+It uses states to track different views instead of routes.
+
+Setup:
+
+1) change app.js according to ui-router specs.
+2) in any controller that requires parameters add $statePares as Dependency Injection (replace with $routeParams).
+3) Major refactor of templates (separate as better as possible).
+
+/********************************** /WEEK 3 **********************************************************/
+
+
+
+
+
+/*****************************************************************************************************/
+/********************************** WEEK 4 ***********************************************************/
+
+/* Client-Server Communication and Angular Testing */
+
+
+/********* Client-Server Communication
+
+1) Install json-server (sudo npm install json-server -g)
+2) Everything in conFusion/app moved to public folder in order to be functional with the json-server, including
+   the bower_components.
+3) Start the server (json-server --watch db.json)
+
+
+/********* Angular $http Service
+
+
+
+/********************************** /WEEK 4 *********************************************************/
---
+++ week-4/json-server/public/scripts/controllers.js
@@ -0,0 +1,176 @@
+/**
+ * Created by christos on 9/5/2016.
+ */
+'use strict';
+
+angular.module('confusionApp')
+
+    // MANY DISHES CONTROLLER WITH CATEGORY FILTER OPTION (tabs)
+    .controller('MenuController', [ '$scope', 'menuFactory', function($scope, menuFactory) {
+        $scope.filtText = '';
+        $scope.tab = 1;
+        $scope.showDetails = false;
+
+        $scope.dishes = menuFactory.getDishes();
+
+        $scope.select = function(setTab){
+            $scope.tab = setTab;
+
+            if (setTab === 2)
+                $scope.filtText = "appetizer";
+            else if (setTab === 3)
+                $scope.filtText = "mains";
+            else if (setTab === 4)
+                $scope.filtText = "dessert";
+            else
+                $scope.filtText = "";
+        };
+
+        $scope.isSelected = function (checkTab) {
+            return ($scope.tab === checkTab);
+        };
+
+        $scope.toggleDetails = function() {
+            // the below toggles the boolean showDetails variable from one value to another value
+            $scope.showDetails = !$scope.showDetails;
+        };
+    }])
+
+    // ONE DISH WITH 5 COMMENTS CONTROLLER
+    .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+        $scope.inputText = "";
+
+        var dish = menuFactory.getDish(parseInt($stateParams.id,10));
+        //
+        $scope.dish = dish;
+
+        // order by indicator
+        $scope.orderingText = "";
+
+        // direction of order indicator (asceding by default)
+        $scope.inWhichOrder = false;
+
+        // FUNCTION THAT CONTROLS THE ORDERING BY INPUT TEXT
+        $scope.textFilter = function() {
+            var input = document.getElementById("inputText").value;
+
+            // ORDER BY RATING
+            if(input === "rating"){          // ascending
+                $scope.orderingText = "rating";
+            }else if(input === "-rating"){   // descending
+                $scope.orderingText = "rating";
+                $scope.inWhichOrder = true;
+            }// ORDER BY DATE
+            else if(input === "date"){       // ascending
+                $scope.orderingText = "date";
+            }else if(input === "-date"){     // descending
+                $scope.orderingText = "date";
+                $scope.inWhichOrder = true;
+            }// ORDER BY AUTHOR
+            else if(input === "author"){     // ascending
+                $scope.orderingText = "author";
+            }else if(input === "-author"){   // descending
+                $scope.orderingText = "author";
+                $scope.inWhichOrder = true;
+            }else{                           // no order (display as it is)
+                $scope.orderingText = "";
+                $scope.inWhichOrder = false;
+            }
+        };  // end of textFilter function
+
+    }])
+
+    // CONTACT CONTROLLER
+    .controller('ContactController', ['$scope', function($scope) {
+
+        $scope.feedback = {contact:"", firstName:"", lastName:"", agree:false, email:"" };
+        var contact_options = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+        $scope.contact_options = contact_options;
+        $scope.invalidChannelSelection = false;
+    }])
+
+    // VALIDATOR CONTACT CONTROLLER
+    .controller('FeedbackController', ['$scope', function($scope) {
+
+        $scope.sendFeedback = function() {
+
+            console.log($scope.feedback);
+
+            if ($scope.feedback.agree && ($scope.feedback.contact == "")&& !$scope.feedback.contact) { // the rule after && might not needed
+                $scope.invalidChannelSelection = true;
+                console.log('incorrect');
+            }
+            else {
+                $scope.invalidChannelSelection = false;
+                $scope.feedback = {contact:"", firstName:"", lastName:"", agree:false, email:"" };
+                $scope.feedback.contact="";
+
+                $scope.feedbackForm.$setPristine();
+                console.log($scope.feedback);
+            }
+        };
+    }])
+
+
+    /*
+     *
+     * ASSIGNMENT 2 - CONTROLLERS
+     *
+     * */
+    .controller('DishCommentController', ['$scope', function($scope) {
+
+        //Step 1: Create a JavaScript object to hold the comment from the form
+        $scope.newComment = {name:"", rating:"5", text_comment:"", date:"" };
+        var newComment = $scope.newComment;
+
+        $scope.submitComment = function () {
+
+            //Step 2: This is how you record the date
+            newComment.date = new Date().toISOString();
+
+            // Step 3: Push your comment into the dish's comment array
+            $scope.dish.comments.push(
+                {   rating:newComment.rating,
+                    comment:newComment.text_comment,
+                    author:newComment.name,
+                    date:newComment.date
+                }
+            );
+
+            console.log(newComment);
+
+            //Step 4: reset your form to pristine
+            $scope.commentForm.$setPristine();
+            //Step 5: reset your JavaScript object that holds your comment
+            $scope.newComment = {name:"", rating:"5", text_comment:"", date:"" };
+
+        }
+    }])
+
+    /*
+    *  ASSIGNMENT 3
+    *
+    * */
+
+    // TASK 2 ------------------------------------//
+     .controller('IndexController', ['$scope', '$stateParams',
+        'menuFactory', 'corporateFactory',
+            function($scope, $stateParams, menuFactory, corporateFactory) {
+
+                //$scope.featuredDish = menuFactory.getDish(parseInt($stateParams.id,10));
+                //$scope.promotion = menuFactory.getPromotion(parseInt($stateParams.id,10));
+                //$scope.chef =  corporateFactory.getLeader(parseInt($stateParams.id,10));
+
+                $scope.featuredDish = menuFactory.getDish(0);
+                $scope.promotion = menuFactory.getPromotion(0);
+                $scope.chef =  corporateFactory.getLeader(0);
+
+
+    }])
+    // TASK 2 ------------------------------------//
+    .controller('AboutController', ['$scope', '$stateParams', 'corporateFactory', function($scope, $stateParams, corporateFactory) {
+        $scope.leaders = corporateFactory.getLeaders();
+
+    }])
+
+;
